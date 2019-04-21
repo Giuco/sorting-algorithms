fbff1ec47f627bb6838f9c862560b7dcf4ff277f
Giuco <giucoferrari@gmail.com>
2019-04-21 19:06:29 -0300

    Structuring quick and bubble sorting

--- sorting_algorithms/__init__.py
+++ sorting_algorithms/__init__.py
@@ -1,3 +1,5 @@
 from .merge_sort import merge_sort
 from .selection_sort import selection_sort
 from .count_sort import count_sort
+from .bubble_sort import bubble_sort
+from .quick_sort import quick_sort
---
+++ sorting_algorithms/quick_sort.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def quick_sort(elements: List[float]) -> List[float]:
+    raise NotImplementedError
