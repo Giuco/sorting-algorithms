16e115d789c2fa0d908ba8dcf1fd1636a5afc926
Giuliano Ferrari <giuliano.ferrari@nexadigital.com.br>
2019-04-25 13:48:56 -0300

    Done Quick Sort and Quick Sort test

--- sorting_algorithms/quick_sort.py
+++ sorting_algorithms/quick_sort.py
@@ -1,5 +1,34 @@
 from typing import List
+from numbers import Number
+from random import randint
 
 
-def quick_sort(elements: List[float]) -> List[float]:
-    raise NotImplementedError
+def get_random_pivot_point(size: int) -> int:
+    return randint(0, size-1)
+
+
+def get_pivot_point(elements: List[Number]) -> int:
+    return get_random_pivot_point(len(elements))
+
+
+def quick_sort(elements: List[Number]) -> List[Number]:
+    if len(elements) <= 1:
+        return elements
+
+    pivot_point = get_pivot_point(elements)
+    pivot = elements[pivot_point]
+
+    left, middle, right = [], [], []
+
+    for e in elements:
+        if e == pivot:
+            middle.append(e)
+        elif e > pivot:
+            right.append(e)
+        else:
+            left.append(e)
+
+    left = quick_sort(left)
+    right = quick_sort(right)
+
+    return left + middle + right
